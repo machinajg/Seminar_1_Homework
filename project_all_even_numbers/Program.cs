cec276e63e169c3e6ef4c273221d117da6b27fd6
Julia Machina <machinajg@gmail.com>
2023-02-02 17:57:20 +0300

    all even numbers project added

---
+++ project_all_even_numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = 2;
+
+while (numberB < numberA)
+{
+  if (numberB % 2 == 0)
+  Console.WriteLine (numberB);
+  numberB++;
+}
